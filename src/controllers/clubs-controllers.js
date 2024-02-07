80f8e62cf45c89ba8e977965e5c4500591a9ac1d
Paula Arce <118852959+Paula-A21@users.noreply.github.com>
2024-02-07 09:36:55 -0300

    Design club delete view

--- src/controllers/clubs-controllers.js
+++ src/controllers/clubs-controllers.js
@@ -3,7 +3,7 @@ import ClubsModel from "../models/Clubs.js";
 
 const Clubs = ClubsModel(sequelize);
 
-const clubsController = async (req, res) => {
+const crudClubs = async (req, res) => {
   try {
     const clubs = await Clubs.findAll();
     const action = req.query.action;
@@ -13,9 +13,11 @@ const clubsController = async (req, res) => {
       return res.status(404).json({ error: "No clubs were found in the database" });
     }
 
-    const formattedClubs = clubs.map((club) => {
-      const formattedClubName = club.club_name.toLowerCase().replace(/ /g, "_");
-      const imagePath = `/images/crests/${formattedClubName}.png`;
+    const formatted_clubs = clubs.map((club) => {
+
+      const formatted_clubs_name = club.club_name.toLowerCase().replace(/ /g, "");
+      const imagePath = `/images/crests/${formatted_clubs_name}.png`;
+      
       return {
         ...club.toJSON(), 
         imagePath
@@ -24,45 +26,62 @@ const clubsController = async (req, res) => {
 
     switch (action) {
       case "view-all-clubs":
-        return res.render("all-clubs", { clubs: formattedClubs });
+
+        return res.render("all-clubs", { clubs: formatted_clubs });
 
       case "view-club":
+
         const club_detail = await Clubs.findByPk(id_UUID);
+
         if (!club_detail) {
           return res.status(404).json({ error: "The club was not found" });
         }
-
-        const formattedDetailClubName = club_detail.club_name.toLowerCase().replace(/ /g, "_");
+        
+        const formattedDetailClubName = club_detail.club_name.toLowerCase().replace(/ /g, "");
         const imagePathDetail = `/images/crests/${formattedDetailClubName}.png`;
+        
 
         return res.render("detail-club", {
           club_detail,
           imagePathDetail,
         });
 
-      case "update-club":
+        case "update-club":
 
-      // return res.render("update-club", { club_detail });
-
-      case "delete-club":
-        await Clubs.destroy({
-          where: {
-            id: id_UUID,
-          },
-        });
-        return res.redirect("/clubs");
-
-      default:
-        const randomClubs = clubs.sort(() => 0.5 - Math.random()).slice(0, 3);
-        return res.render("home", {
-          clubs: randomClubs,
-          deleteClubPartial: "delete-club",
-        });
+        // return res.render("update-club", { club_detail });
+  
+        case "delete-club":
+  
+          const deleted_club_count = await Clubs.destroy({
+            where: {
+              id: id_UUID
+            },
+          });
+  
+          if (deleted_club_count > 0) {
+            // El club se eliminó correctamente
+  
+            return res.render("delete-club", {});
+  
+          } else {
+            // No se encontró el club para eliminar
+            return res.status(404).json({ error: "Club not found or could not be deleted" });
+          }
+  
+  
+        default:
+  
+          const random_clubs = clubs.sort(() => 0.5 - Math.random()).slice(0, 3);
+  
+          return res.render("home", {
+            clubs: random_clubs,
+            deleteClubPartial: "delete-club"
+          });
+      }
+    } catch (error) {
+      console.log(error.message);
+      return res.status(500).json({ error: "An error has occurred" });
     }
-  } catch (error) {
-    console.log(error.message);
-    return res.status(500).json({ error: "An error has occurred" });
-  }
-};
-
-export { clubsController };
+  };
+  
+  export { crudClubs };
--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,14 +1,14 @@
 import { Router } from "express";
 const router = Router();
 
-import { clubsController } from "../controllers/clubs-controllers.js";
+import { crudClubs } from "../controllers/clubs-controllers.js";
 
 
 
-router.get('/clubs', clubsController);
-router.get('/clubs/:id_UUID', clubsController);
-router.post('/clubs', clubsController);
-router.delete('/clubs/:id_UUID', clubsController);
-router.put('/clubs/:id_UUID', clubsController);
+router.get('/clubs', crudClubs);
+router.get('/clubs/:id_UUID', crudClubs);
+router.post('/clubs', crudClubs);
+router.delete('/clubs/:id_UUID', crudClubs);
+router.put('/clubs/:id_UUID', crudClubs);
 
 export default router;
